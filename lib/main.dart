e371683dba68c792afd1f3399145ffce16d53634
JAN_ADRIAN_SOSA_CORDERO <a.22308051281317@cbtis128.edu.mx>
2025-02-19 20:33:32 +0000

    enviar practica6

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
@@ -8,8 +8,9 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       // Application name
-      title: 'Flutter Hello World',
+      title: 'SosaCordero',
       // Application theme data, you can set the colors for the application as
       // you want
       theme: ThemeData(
@@ -17,25 +18,42 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
       ),
       // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Sosa appBar'),
     );
   }
 }
 
 class MyHomePage extends StatelessWidget {
   final String title;
-  const MyHomePage({super.key, required this.title});  
+  const MyHomePage({super.key, required this.title});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search),
+            onPressed: () {},
+          ),
+          IconButton(
+            icon: const Icon(
+              Icons.more_vert,
+            ),
+            onPressed: () {},
+          )
+        ],
+        backgroundColor: Colors.orangeAccent,
+
         // The title text which will be shown on the action bar
         title: Text(title),
+        leading: const Icon(Icons.bike_scooter),
+        centerTitle: true,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      // ignore: prefer_const_constructors
+      body: const Center(
+        child: const Text(
+          'Hello, JAN ADRIAN SOSA CORDERO CON APPBAR!',
         ),
       ),
     );
